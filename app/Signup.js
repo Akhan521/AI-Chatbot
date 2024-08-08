3b342dcf3cfb4594f0572d31faa57e735d2a61d1
Falak Tulsi <falaktulsi@gmail.com>
2024-08-07 21:50:36 -0700

    Added User Auth

---
+++ app/Signup.js
@@ -0,0 +1,92 @@
+"use client";
+
+import React, { useState } from "react";
+import { auth, firestore } from "./api/firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
+import { Link, useNavigate } from "react-router-dom";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState(null);
+  // const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isStrongPassword(password)) {
+      setError(
+        "Password must be at least 8 characters long and contain uppercase, lowercase, number, and special character"
+      );
+      return;
+    }
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await setDoc(doc(firestore, "users", userCredential.user.uid), {
+        username,
+        email,
+      });
+      // navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const isStrongPassword = (password) => {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasNonalphas = /\W/.test(password);
+    return (
+      password.length >= minLength &&
+      hasUpperCase &&
+      hasLowerCase &&
+      hasNumbers &&
+      hasNonalphas
+    );
+  };
+
+  return (
+    <div className="signup">
+      <main>
+        <h2>Sign Up</h2>
+        {error && <p className="error">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+        {/* <p>
+          Already have an account? <Link to="/login">Login</Link>
+        </p> */}
+      </main>
+    </div>
+  );
+}
+
+export default Signup;
